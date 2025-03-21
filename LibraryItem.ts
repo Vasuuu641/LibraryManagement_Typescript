f2d8fd7e1465ccc5c76f5bdae71fc9f0cb27fdab
Vasundhara Ravikumar <vasundhararavi641@gmail.com>
2025-03-21 10:21:21 +0100

    library_item_creation
    Created the abstract class LibraryItem

---
+++ LibraryItem.ts
@@ -0,0 +1,37 @@
+abstract class LibraryItem {
+    constructor(
+        public title: string,
+        public author: string,
+        public publication_year: number,
+        private item_id: number,
+        protected is_available: boolean
+    ) {
+    }
+
+    //Methods
+    //1. Display_Info - Prints details of the item
+    Display_Info(): void {
+        console.log(`Title: ${this.title}`);
+        console.log(`Author: ${this.author}`);
+        console.log(`Publication Year: ${this.publication_year}`);
+        console.log(`Item ID: ${this.item_id}`);
+        console.log(`Is Available: ${this.is_available}`);
+    }
+
+    //2. Borrow_Item - Set is_available to false if available otherwise raise an exception
+    Borrow_Item(): void{
+        if(this.is_available)
+        {
+            this.is_available = false;
+            console.log("Sorry, this book isn't available at the moment :(");
+        }
+    }
+
+    //3. Return_Item - Sets is_available to true when book is borrowed
+    Return_Item(): void{
+        if(!this.is_available){
+            this.is_available = true;
+            console.log("Thank you for returning this book!");
+        }
+    }
+}
